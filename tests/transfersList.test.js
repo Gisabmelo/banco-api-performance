233138b39b19fc2ff02cfaa276edb415cec5e8c0
Rafael Zanella <rafaelzanella17@gmail.com>
2025-10-10 09:29:33 -0300

    tests(transfersList): atualizar testes com status 200 e 403

--- tests/transfersList.test.js
+++ tests/transfersList.test.js
@@ -11,12 +11,40 @@ export const options = {
 }
 
 export default function () {
-  const res = http.get('http://localhost:3000/transfers')
+  const loginUser = `http://localhost:3000/users/login`
+  
+  const loginPayload = JSON.stringify({ username: 'julio', password: '123456', })
+  const loginParams = { headers: { 'Content-Type': 'application/json' }, }
+  
+  const loginResponse = http.post(loginUser, loginPayload, loginParams)  
+  const token = loginResponse.json('token')
+  
+  // Verifica se o login foi bem-sucedido
+  check(loginResponse, {
+    'Login com sucesso: status 200 (OK)': (r) => r.status === 200,
+    'Login com sucesso: retornou o token corretamente': (r) => r.json('token') !== undefined,
+  })
+  
+  const transfersEndpoint = `http://localhost:3000/transfers` 
+  const tokenInvalido = `Bearer ${token}error543210`
+  
+  const paramsInvalido = { headers: { 'Content-Type': 'application/json', Authorization: tokenInvalido, }}
+  
+  const res = http.get(transfersEndpoint)
+  const resInvalido = http.get(transfersEndpoint, paramsInvalido)  
 
+  // Consultar rota transfers sem informar token.
   check(res, {
-    'status é 400 ou 401': (r) => r.status === 400 || r.status === 401,
-    'resposta contém mensagem de token': (r) =>
-      r.body.includes('Token não fornecido.') || r.body.includes('Token inválido'),
+    'Consultar transferências sem inserir um token: status 401': (r) => r.status === 401,
+    'Consultar transferências - mensagem de erro: Token não fornecido.': (r) =>
+      r.body.includes('Token não fornecido.'),
+  })
+
+  // Consultar rota transfers com token inválido.
+  check(resInvalido, {
+    'Consultar transferências com token inválido: status 403': (r) => r.status === 403,
+    'Consultar transferências - mensagem de erro: Token inválido.': (r) =>
+      r.body.includes('Token inválido.'),
   })
 
   sleep(1)
