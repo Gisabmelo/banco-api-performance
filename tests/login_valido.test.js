f668d0be369822e3b800644b2c65dfad6694f72b
Gislaine Melo [ Zallpy ] <gislaine.melo@zallpy.com>
2025-10-13 20:47:53 -0300

    Adicionando usuarios virtuais e duration

--- tests/login_valido.test.js
+++ tests/login_valido.test.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-
-    iterations: 10,
-     thresholds: {
+vus:10,
+duration: '30s',
+thresholds: {
     http_req_failed: ['rate<0.01'], 
-    http_req_duration: ['p(90)<100'], 
+    http_req_duration: ['p(90)<3000', 'max <5000'], 
   },
 
 };
